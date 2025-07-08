efb4175876f92cc80a4985fe99abd95d7c189f0c
dx4homelab <145386996+dx4homelab@users.noreply.github.com>
2025-07-08 16:08:19 -0600

    attmept to fix chrome not instaslled to image by removing old GPG key

--- build_files/dx/02-enable-chrome-yum-repo.sh
+++ build_files/dx/02-enable-chrome-yum-repo.sh
@@ -10,16 +10,16 @@ echo "enabled=1" >> /etc/yum.repos.d/google-chrome.repo
 # Google keys in the RPM database.  Will be deleted soon.
 
 # First, delete all old keys; see https://github.com/rpm-software-management/rpm/issues/2577
-# echo "Fixing issues with Google GPG keys"
-# set +oue
-# rpm -qa gpg-pubkey* --qf '%{NAME}-%{VERSION}-%{RELEASE} %{PACKAGER}\n' | grep 'linux-packages-keymaster@google.com' | sed 's/ .*$//' | xargs
-# GOOGLE_PUBKEYS_RPMS=$(rpm -qa gpg-pubkey* --qf '%{NAME}-%{VERSION}-%{RELEASE} %{PACKAGER}\n' | grep 'linux-packages-keymaster@google.com' | sed 's/ .*$//' | xargs)
-# set -oue
-# echo "Installed pubkeys RPMS are $GOOGLE_PUBKEYS_RPMS"
-# if [ -n "$GOOGLE_PUBKEYS_RPMS" ]; then
-#     echo "Removing pakcages $GOOGLE_PUBKEYS_RPMS"
-#     rpm -e $GOOGLE_PUBKEYS_RPMS
-# fi
+echo "Fixing issues with Google GPG keys"
+set +oue
+rpm -qa gpg-pubkey* --qf '%{NAME}-%{VERSION}-%{RELEASE} %{PACKAGER}\n' | grep 'linux-packages-keymaster@google.com' | sed 's/ .*$//' | xargs
+GOOGLE_PUBKEYS_RPMS=$(rpm -qa gpg-pubkey* --qf '%{NAME}-%{VERSION}-%{RELEASE} %{PACKAGER}\n' | grep 'linux-packages-keymaster@google.com' | sed 's/ .*$//' | xargs)
+set -oue
+echo "Installed pubkeys RPMS are $GOOGLE_PUBKEYS_RPMS"
+if [ -n "$GOOGLE_PUBKEYS_RPMS" ]; then
+    echo "Removing packages $GOOGLE_PUBKEYS_RPMS"
+    rpm -e $GOOGLE_PUBKEYS_RPMS
+fi
 
 # We need to download and install the Google signing keys separately, we can't trust
 # rpm-ostree to do it cleanly from the yum repo directly.
@@ -28,4 +28,8 @@ echo "enabled=1" >> /etc/yum.repos.d/google-chrome.repo
 echo "Downloading Google Signing Key"
 curl https://dl.google.com/linux/linux_signing_key.pub > /tmp/linux_signing_key.pub
 
+echo "Importing Google Signing Key ..."
+
 rpm --import /tmp/linux_signing_key.pub
+
+echo "Done importing Google Signing Key."
