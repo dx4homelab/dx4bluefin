9a2c4479d5dacd89310e58d743472feee625e3f2
dx4homelab <145386996+dx4homelab@users.noreply.github.com>
2025-07-08 12:17:28 -0600

    readding TARGET_IMAGE_REGISTRY fix

--- mods/yq-validation.sh
+++ mods/yq-validation.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #
 # copy original reusable-build.yml to original-workflows
-cp -v original-reusable-build.yml original-workflows/reusable-build.yml
+cp -vf original-reusable-build.yml reusable-build.yml
 #
-yq eval -v -i --from-file yq-mods4reusable-build-yaml.yq original-workflows/reusable-build.yml
+yq eval -v -i --from-file yq-mods4reusable-build-yaml.yq reusable-build.yml
+
+echo grep result ...
+grep 'TARGET_IMAGE_REGISTRY' -b2 -a2 reusable-build.yml
 
 # restore original packages.json before running yq
 # cp original-packages.json original-bluefin/packages.json
