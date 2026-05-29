1aeac42a30e0c3fa78c9a6c18c9c9de6f11b3684
dx4homelab <dx4homelab@users.noreply.github.com>
2026-05-29 10:40:55 +0000

    chore: automated customization of upstream bluefin/dx build workflow files

--- build_files/dx/00-dx.sh
+++ build_files/dx/00-dx.sh
@@ -139,7 +139,6 @@ fi
 
 systemctl enable docker.socket
 systemctl enable podman.socket
-systemctl enable swtpm-workaround.service
 systemctl enable libvirt-workaround.service
 systemctl enable bluefin-dx-groups.service
 systemctl enable snapd.socket
